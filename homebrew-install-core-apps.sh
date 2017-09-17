9104bd2c469dda472bd5d8793641403b6c3029a9
Mike Bydalek <mibydale@cisco.com>
2017-09-17 08:57:29 -0700

    Initial add

---
+++ homebrew-install-core-apps.sh
@@ -0,0 +1,23 @@
+brew cask install caffeine
+brew cask install dropbox
+brew cask install evernote
+brew cask install firefox
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install little-snitch
+brew cask install omnigraffle
+brew cask install postman
+brew cask install spectacle
+brew cask install spotify
+brew cask install sublime-text
+brew cask install teamviewer
+brew cask install the-unarchiver
+brew cask install unison
+brew cask install utorrent
+brew cask install vmware-remote-console
+brew cask install docker
+brew cask install docker-toolbox
+brew install macvim
+brew install dockutil
+brew install gpg
+brew cask install java
